aaa9f2a24645b848aba2a885f1a414b7ec46c1e4
Kravataf <jurajsoltyss@gmail.com>
2024-09-21 18:42:41 +0200

    Update menumaker.js

--- menumaker.js
+++ menumaker.js
@@ -17,7 +17,25 @@ const template = [
         label: 'File',
         submenu: [
             {
-                label: 'Open File',
+                label: 'Create Project',
+                click: async () => {
+                    //call func here :p
+                }
+            },
+            {
+                label: 'Open Project',
+                click: async () => {
+                    //call func here :p
+                }
+            },
+            {
+                label: 'Import Asset',
+                click: async () => {
+                    //call func here :p
+                }
+            },
+            {
+                label: 'Export Project',
                 click: async () => {
                     //call func here :p
                 }
@@ -38,9 +56,44 @@ const template = [
                 click: async () => {
                     //call func here :p
                 }
+            },
+            {
+                label: 'Cut',
+                click: async () => {
+                    //call func here :p
+                }
+            },
+            {
+                label: 'Copy',
+                click: async () => {
+                    //call func here :p
+                }
+            },
+            {
+                label: 'Paste',
+                click: async () => {
+                    //call func here :p
+                }
+            },
+            {
+                label: 'Delete',
+                click: async () => {
+                    //call func here :p
+                }
             }
         ]
-    }
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Open Inspector',
+                click: async () => {
+                    win.webContents.openDevTools()
+                }
+            }
+        ]
+    },
 ]
 
 module.exports.mainMenu = Menu.buildFromTemplate(template);
